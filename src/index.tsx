f696dee5cc4086ff4f946dce597deee42721909c
Christine Jordan <christine.jordan@shopify.com>
2018-06-05 16:17:52 -0400

    Update index.tsx for exercise4

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,8 @@
 // Now, ready to do you some TypeScript?!
 
 // ======== Exercise 1: Basic types ========
-import exercise1 from './Exercise_1_Basic_Types/exercise1';
-exercise1();
+// import exercise1 from './Exercise_1_Basic_Types/exercise1';
+// exercise1();
 
 // ======== Exercise 2: Interfaces ==========
 // import exercise2 from './Exercise_2_Interfaces/exercise2';
@@ -20,8 +20,8 @@ exercise1();
 // exercise3();
 
 // ======== Exercise 4: Types =========
-// import exercise4 from './Exercise_4_Types/exercise4';
-// exercise4();
+import exercise4 from './Exercise_4_Types/exercise4';
+exercise4();
 
 // ======== Exercise 5: Code flow  =========
 // import exercise5 from './Exercise_5_Classes/exercise5';
