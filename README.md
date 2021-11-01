9810b936e59114bddcad90e2cb8aec4485bcfec0
Grigol Tchkoidze <grigol.tchkoidze@quantori.com>
2021-11-01 12:14:24 +0400

    refactor: removed unnecessary files

--- README.md
+++
@@ -1,4 +0,0 @@
-# ts-sandbox
-TypeScript workshop exercises
-
-This repo is a fork of [asalisbury/ts-sandbox](https://github.com/asalisbury/ts-sandbox).
--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,8 @@
 // Now, ready to do you some TypeScript?!
 
 // ======== Exercise 1: Basic types ========
-import exercise1 from './Exercise_1_Basic_Types/exercise1';
-exercise1();
+// import exercise1 from './Exercise_1_Basic_Types/exercise1';
+// exercise1();
 
 // ======== Exercise 2: Interfaces ==========
 // import exercise2 from './Exercise_2_Interfaces/exercise2';
@@ -26,28 +26,3 @@ exercise1();
 // ======== Exercise 5: Code flow  =========
 // import exercise5 from './Exercise_5_Classes/exercise5';
 // exercise5();
-
-
-// ⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇
-//   Solutions
-// ⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈
-
-// ======== Solution 1: Basic types ========
-// import solution1 from './Exercise_1_Basic_Types/solution1';
-// solution1();
-
-// ======== Solution 2: Functions ==========
-// import solution2 from './Exercise_2_Interfaces/solution2';
-// solution2();
-
-// ======== Solution 3: Classes ============
-// import solution3 from './Exercise_3_Functions/solution3';
-// solution3();
-
-// ======== Solution 4: Interfaces =========
-// import solution4 from './Exercise_4_Types/solution4';
-// solution4();
-
-// ======== Solution 5: Code flow  =========
-// import solution5 from './Exercise_5_Classes/solution5';
-// solution5();
